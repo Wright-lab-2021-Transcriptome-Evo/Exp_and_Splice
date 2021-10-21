489088fa3adc694f15a867753846efe2cf39cc37
petedprice <petedprice@gmail.com>
2021-10-21 11:40:33 +0100

    SALMON INDEX

---
+++ Scripts/Salmon/Step_1_salmon_index.sh
@@ -0,0 +1,27 @@
+### THE FIRST AND ONLY INPUT IS THE CDS FILE THAT YOU'RE GOING TO USE WITH YOUR SAMPLES IN SALMON ----
+
+#!/bin/bash
+
+# Request one hour of wallclock time
+#$ -l h_rt=8:0:0
+
+# Request RAM
+#$ -l rmem=5G
+
+# Select threads
+#$ -pe smp 4
+
+# Set the working directory
+#$ -wd /fastdata/bop20pp/comp_exp_models/wdir/
+
+# Run the application.
+
+echo "started"
+date
+source /usr/local/extras/Genomics/.bashrc
+
+source activate salmon
+
+out=${1%*.cds.fa}_salmon_index
+salmon index -t $1 -i $out
+date
